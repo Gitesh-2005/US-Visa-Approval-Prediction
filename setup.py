fa9fdbfb9a5f90fa4201826cd690f2c6817cd3b9
Gitesh Mohaniraj Tamboli <giteshtamboli0@gmail.com>
2025-07-16 19:04:39 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="US Visa",
+    version="0.0.0",
+    author="Gitesh",
+    author_email="giteshtamboli0@gmail.com",
+    packages=find_packages() 
+)
